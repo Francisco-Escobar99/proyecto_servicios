991fead9653958aa7b94a4e4b74dd347b4f48e6f
Ale Espinosa <espinosacordovaaleff@gmail.com>
2023-07-20 22:55:34 -0600

    fix version 1.5

--- lib/features/authentication/pages/login/login_resgisterBase.dart
+++ lib/features/authentication/pages/login/login_resgisterBase.dart
@@ -11,13 +11,19 @@ class RegisterCustomer extends StatefulWidget {
 }
 
 class _RegisterCustomerState extends State<RegisterCustomer> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _lastnameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _repeatPasswordController =
       TextEditingController();
-  final bool _isPasswordVisible = false;
+  late bool _isPasswordVisible = false;
 
   @override
   void dispose() {
+    _emailController.dispose();
+    _nameController.dispose();
+    _lastnameController.dispose();
     _passwordController.dispose();
     _repeatPasswordController.dispose();
     super.dispose();
@@ -69,6 +75,7 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                   SizedBox(height: _responsiveHeight(context, 0.04)),
                   TextFormField(
                     autovalidateMode: AutovalidateMode.disabled,
+                    controller: _nameController,
                     decoration: InputDecoration(
                       hintText: 'Nombre',
                       fillColor: HexColor('#FFFFFF'),
@@ -95,6 +102,7 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                   SizedBox(height: _responsiveHeight(context, 0.02)),
                   TextFormField(
                     autovalidateMode: AutovalidateMode.disabled,
+                    controller: _lastnameController,
                     decoration: InputDecoration(
                       hintText: 'Apellidos',
                       fillColor: HexColor('#FFFFFF'),
@@ -121,6 +129,7 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                   SizedBox(height: _responsiveHeight(context, 0.02)),
                   TextFormField(
                     autovalidateMode: AutovalidateMode.disabled,
+                    controller: _emailController,
                     decoration: InputDecoration(
                       hintText: 'Email',
                       fillColor: HexColor('#FFFFFF'),
@@ -144,10 +153,10 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                       ),
                     ),
                   ),
-                  
                   SizedBox(height: _responsiveHeight(context, 0.06)),
                   TextFormField(
-                    obscureText: true, // Ocultar/ver contraseña
+                    obscureText: !_isPasswordVisible,
+                    controller: _passwordController,
                     autovalidateMode: AutovalidateMode.disabled,
                     decoration: InputDecoration(
                       hintText: 'Contraseña',
@@ -170,11 +179,25 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                         borderRadius: BorderRadius.circular(30),
                         borderSide: const BorderSide(color: Colors.transparent),
                       ),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                        child: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(height: _responsiveHeight(context, 0.02)),
                   TextFormField(
-                    obscureText: true, // Ocultar/ver contraseña
+                    obscureText: !_isPasswordVisible,
+                    controller: _repeatPasswordController,
                     autovalidateMode: AutovalidateMode.disabled,
                     decoration: InputDecoration(
                       hintText: 'Repetir contraseña',
@@ -199,10 +222,14 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                       ),
                       suffixIcon: GestureDetector(
                         onTap: () {
-                          // Cambiar la visibilidad de la contraseña
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
                         },
-                        child: const Icon(
-                          Icons.visibility,
+                        child: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
                           color: Colors.grey,
                         ),
                       ),
@@ -213,33 +240,22 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
                     padding: const EdgeInsets.only(left: 10, top: 90),
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Login_selectRole()),
-                        );
+                        _registerUser();
                       },
                       style: ElevatedButton.styleFrom(
-                        // ignore: deprecated_member_use
-                        primary: HexColor('#3B3936'),
-                        // Color de fondo del botón
-                        // ignore: deprecated_member_use
-                        onPrimary: Colors.white,
-                        // Color del texto del botón
+                        foregroundColor: Colors.white,
+                        backgroundColor: HexColor('#3B3936'),
                         textStyle: TextStyle(
                           fontSize: _responsiveTextSize(context, 17),
                           fontWeight: FontWeight.bold,
                         ),
                         minimumSize: Size(_responsiveWidth(context, 0.9),
                             _responsiveHeight(context, 0.07)),
-                        // Ancho y alto mínimos del botón
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30),
                           side: BorderSide(
                             color: HexColor('#3B3936'),
-                            // Color del borde del botón
                             width: 12,
-                            // Ancho del borde del botón
                           ),
                         ),
                       ),
@@ -266,5 +282,61 @@ class _RegisterCustomerState extends State<RegisterCustomer> {
   double _responsiveTextSize(BuildContext context, double size) {
     return size * MediaQuery.of(context).textScaleFactor;
   }
-  // ... Resto del código de _RegisterCustomerState
+
+  // Método para realizar la petición POST
+  void _registerUser() async {
+    String name =
+        _nameController.text; // Obtén el valor del nombre del TextFormField
+    String lastName = _lastnameController
+        .text; // Obtén el valor del apellido del TextFormField
+    String email =
+        _emailController.text; // Obtén el valor del email del TextFormField
+    String password = _passwordController.text;
+    String repeatPassword = _repeatPasswordController.text;
+
+    if (password == repeatPassword) {
+      // Las contraseñas son iguales, realiza la petición POST
+      try {
+        var response = await Dio().post(
+            'https://spokda7of4.execute-api.us-east-1.amazonaws.com/auth-services/auth/register',
+            data: {
+              'email': email,
+              'password': password,
+              'name': name,
+              'last_name': lastName,
+              'type_users_id': 2,
+            });
+            
+        // Aquí puedes manejar la respuesta del servidor si es necesario
+        print('Respuesta del servidor: ${response.data}');
+        print('Respuesta del servidor: ${response.statusCode}');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Login_selectRole()),
+        );
+      } catch (e) {
+        // Manejo de errores en la petición
+        print('Error en la petición: $e');
+      }
+    } else {
+      // Las contraseñas no son iguales, muestra un mensaje de error
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Las contraseñas ingresadas no coinciden.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 }
